6453694acd8aabb6abc5ba8d0ee4c603bf734b0d
Peter Kreppein <pmkreppein@gmail.com>
2019-01-14 06:45:46 +0000

    Automatically backed up by Learn

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".js-more").on('click', function() {
+    let id = $(this).data("id");
+    $.get("/posts/" + id + "/body", function(data) {
+  
+      $("#body-" + id).text(data)
+    })
+  })
+})
